7412b735079a2eb0269c5100457dbe3bac0b7532
sijie.li <lsj86@qq.com>
2015-12-25 17:45:19 +0800

    修正错误

--- system/Core/Lib/Tree.php
+++ system/Core/Lib/Tree.php
@@ -4,152 +4,157 @@ namespace Core\Lib;
 
 class Tree
 {
-    /**
-     * 每个节点的数据
-     * @var array
-     */
-    protected $data = array();
+	/**
+	 * 每个节点的数据
+	 * @var array
+	 */
+	protected $data = array();
 	/**
 	 * 保存每个父节点下面的子节点ID列表
 	 * @var array
 	 */
-    protected $child = array();
+	protected $child = array();
 	/**
 	 * 保存每个节点ID对应的父节点ID
 	 * @var array
 	 */
-    protected $parent = array();
-	/**
-	 * 每个节点层数
-	 * @var array
-	 */
-    protected $layer = array();
+	protected $parent = array();
 	/**
 	 * 空格的HTML实体
 	 * @var string
 	 */
-    protected $space = '&nbsp;';
+	protected $space = '&nbsp;';
 
 	protected $icon = array('│', '├─ ', '└─ ');
 
-    /**
-     * 增加一个节点
-     *
-     * 添加节点一定要先添加父节点，再添加子节点
-     *
-     * @param int $id 节点ID
-     * @param int $parentId 父级ID
-     * @param array $data 节点数据
-     */
-    public function addNode($id, $parentId, $data)
-    {
-        $this->data[$id] = (array)$data;
-        $this->child[$parentId][] = $id;
-        $this->parent[$id] = $parentId;
-        if (!isset($this->layer[$parentId])) {
-            $this->layer[$id] = 0;
-        } else {
-            $this->layer[$id] = $this->layer[$parentId] + 1;
-        }
-    }
+	/**
+	 * 增加一个节点
+	 *
+	 * @param int $id 节点ID
+	 * @param int $parentId 父级ID
+	 * @param array $data 节点数据
+	 */
+	public function addNode($id, $parentId, $data)
+	{
+		$this->data[$id] = (array)$data;
+		$this->child[$parentId][] = $id;
+		$this->parent[$id] = $parentId;
+	}
 
-    /**
-     * 根据节点ID返回节点数据
-     *
-     * @param int $id
-     * @return null|array
-     */
-    public function getValue($id)
-    {
-        return isset($this->data[$id]) ? $this->data[$id] : null;
-    }
+	/**
+	 * 根据节点ID返回节点数据
+	 *
+	 * @param int $id
+	 * @return null|array
+	 */
+	public function getValue($id)
+	{
+		return isset($this->data[$id]) ? $this->data[$id] : null;
+	}
 
-    /**
-     * 获取某个节点的子节点ID列表
-     *
-     * @param int $id
-     * @return array
-     */
-    public function getChildren($id)
-    {
-        return isset($this->child[$id]) ? $this->child[$id] : array();
-    }
+	/**
+	 * 获取某个节点的子节点ID列表
+	 *
+	 * @param int $id
+	 * @return array
+	 */
+	public function getChildren($id)
+	{
+		return isset($this->child[$id]) ? $this->child[$id] : array();
+	}
 
-    /**
-     * 获取某个节点的子节点（包括子节点的子节点）ID列表
-     *
-     * @param int $id
-     * @return array
-     */
-    public function getDeepChildren($id)
-    {
-        $list = array();
-        if (isset($this->child[$id])) {
-            foreach ($this->child[$id] as $cid) {
-                $list[] = $cid;
-                if (isset($this->child[$cid])) {
-                    $list = array_merge($list, $this->getDeepChildren($cid));
-                }
-            }
-        }
-        return $list;
-    }
+	/**
+	 * 获取某个节点的子节点（包括子节点的子节点）ID列表
+	 *
+	 * @param int $id
+	 * @return array
+	 */
+	public function getDeepChildren($id)
+	{
+		$list = array();
+		if (isset($this->child[$id])) {
+			foreach ($this->child[$id] as $cid) {
+				$list[] = $cid;
+				if (isset($this->child[$cid])) {
+					$list = array_merge($list, $this->getDeepChildren($cid));
+				}
+			}
+		}
+		return $list;
+	}
 
-    /**
-     * 返回某个节点的父节点ID
-     *
-     * @param int $id
-     * @return int
-     */
-    public function getParentId($id)
-    {
-        return $this->parent[$id];
-    }
+	/**
+	 * 返回某个节点的父节点ID
+	 *
+	 * @param int $id
+	 * @return int
+	 */
+	public function getParentId($id)
+	{
+		return $this->parent[$id];
+	}
 
-    /**
-     * 返回各层父ID列表
-     *
-     * @param int $id
-     * @return array
-     */
-    public function getDeepParentIds($id)
-    {
-        $list = array();
-        if ($this->parent[$id] > 0) {
-            $list[] = $pid = $this->parent[$id];
-            if ($this->parent[$pid] > 0) {
-                $list = array_merge($list, $this->getDeepParentIds($pid));
-            }
-        }
-        return $list;
-    }
+	/**
+	 * 返回各层父ID列表
+	 *
+	 * @param int $id
+	 * @return array
+	 */
+	public function getDeepParentIds($id)
+	{
+		$list = array();
+		if ($this->parent[$id] > 0) {
+			$list[] = $pid = $this->parent[$id];
+			if ($this->parent[$pid] > 0) {
+				$list = array_merge($list, $this->getDeepParentIds($pid));
+			}
+		}
+		return $list;
+	}
 
-    /**
-     * 获取某个节点下的子节点列表
-     *
-     * @param array $list
-     * @param int $root
-     * @return array
-     */
-    public function getList(&$list, $root = 0)
-    {
-        if (isset($this->child[$root])) {
-            foreach ($this->child[$root] as $id) {
-                $list[] = $id;
-                if (isset($this->child[$id])) {
-                    $this->getList($list, $id);
-                }
-            }
-        }
-    }
+	/**
+	 * 获取某个节点下的子节点列表
+	 *
+	 * @param array $list
+	 * @param int $root
+	 * @return array
+	 */
+	public function getList(&$list, $root = 0)
+	{
+		if (isset($this->child[$root])) {
+			foreach ($this->child[$root] as $id) {
+				$list[] = $id;
+				if (isset($this->child[$id])) {
+					$this->getList($list, $id);
+				}
+			}
+		}
+	}
 
-    public function getLayer($id, $s = '├─&nbsp;', $before = '')
-    {
-        if ($this->layer[$id] > 0) {
-            return $before . str_repeat($this->space, $this->layer[$id] * 4) . $s;
-        }
-        return $before;
-    }
+	/**
+	 * 获取某个节点的层级数
+	 *
+	 * @param int $id
+	 * @return int
+	 */
+	public function getLayer($id) {
+		$n = 0;
+		$nodeId = $id;
+		while ($this->parent[$nodeId] > 0) {
+			$nodeId = $this->parent[$nodeId];
+			$n ++;
+		}
+		return $n;
+	}
+
+	public function getLayerHtml($id, $space = '', $s = '├─&nbsp;')
+	{
+		$layer = $this->getLayer($id);
+		if ($layer > 0) {
+			return str_repeat($space, $layer) . $s;
+		}
+		return '';
+	}
 
 	/**
 	 * 返回一个树形结构HTML
@@ -163,38 +168,38 @@ class Tree
 	 * @param string $selStr
 	 * @return string
 	 */
-    public function makeTreeHtml($rootId, $selectedId, $str, $groupStr = '', $selStr = 'selected')
-    {
-        $childIds = $this->getChildren($rootId);
-        $_count = count($childIds);
-	    $result = '';
-        if ($_count > 0) {
-            foreach ($childIds as $_key => $_id) {
-                $data = $this->getValue($_id);
+	public function makeTreeHtml($rootId, $selectedId, $str, $groupStr = '', $selStr = 'selected')
+	{
+		$childIds = $this->getChildren($rootId);
+		$_count = count($childIds);
+		$result = '';
+		if ($_count > 0) {
+			foreach ($childIds as $_key => $_id) {
+				$data = $this->getValue($_id);
 
-                if ($_count == ($_key + 1)) {
-                    $spacer = $this->getLayer($_id, $this->icon[2]); //└─
-                } else {
-                    $spacer = $this->getLayer($_id, $this->icon[1]); //├─
-                }
+				if ($_count == ($_key + 1)) {
+					$spacer = $this->getLayerHtml($_id, '|&nbsp;&nbsp;&nbsp;&nbsp;', $this->icon[2]); //└─
+				} else {
+					$spacer = $this->getLayerHtml($_id, '|&nbsp;&nbsp;&nbsp;&nbsp;', $this->icon[1]); //├─
+				}
 
-                $selected = ($_id == $selectedId) ? $selStr : '';
-                @extract($data);
-	            $string = '';
+				$selected = ($_id == $selectedId) ? $selStr : '';
+				@extract($data);
+				$string = '';
 
-	            if ($groupStr && $this->getParentId($_id) > 0) {
-		            eval("\$string = \"{$groupStr}\";");
-	            } else {
-		            eval("\$string = \"{$str}\";");
-	            }
+				if ($groupStr && $this->getParentId($_id) > 0) {
+					eval("\$string = \"{$groupStr}\";");
+				} else {
+					eval("\$string = \"{$str}\";");
+				}
 
-	            $result .= $string;
-                if (isset($this->child[$_id])) {
-	                $result .= $this->makeTreeHtml($_id, $selectedId, $str, $groupStr, $selStr);
-                }
-            }
-        }
-        return $result;
-    }
+				$result .= $string;
+				if (isset($this->child[$_id])) {
+					$result .= $this->makeTreeHtml($_id, $selectedId, $str, $groupStr, $selStr);
+				}
+			}
+		}
+		return $result;
+	}
 
 }
