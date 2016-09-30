17c286ccc91d7248274ff8ffba33c2d16c1ed970
sijie.li <lsj86@qq.com>
2016-09-30 18:37:33 +0800

    添加注释

--- system/Core/Environment.php
+++ system/Core/Environment.php
@@ -4,8 +4,23 @@ namespace Core;
 use Core\Exception\CoreException;
 
 /**
- * 运行环境
- * 
+ * 运行环境控制
+ *
+ * 项目从开发到发布一般都需要经过以下步骤：
+ * 开发人员在本地进行功能开发 -> 完成开发提交给测试人员测试 -> 发布到预发布环境进行功能检查 -> 上线
+ *
+ * 对应的运行环境为：
+ * - development 开发环境
+ * - testing 测试环境
+ * - pre_release 预发布环境
+ * - production 生产环境
+ *
+ * 为了让运行环境的配置不入侵到代码中，这里支持两种形式的设置：
+ * 1. 通过配置服务器环境变量来指定，代码中通过 $_SERVER[var] 获取。适用于运行在http模式。
+ *    在nginx中可以使用 `fastcgi_param ENVIRONMENT development;` 指令来配置一个名为 ENVIRONMENT，值
+ *    为 development 的环境变量。apache服务器可以在VirtualHost配置中使用 `SetEnv ENVIRONMENT development` 进行设置。
+ * 2. 将当前的环境名称写入到一个外部文件中，然后通过读取这个外部文件内容来获取当前的运行环境。这个适用于运行在命令行模式。
+ *
  * @author lisijie <lsj86@qq.com>
  * @package Core
  */
@@ -111,6 +126,10 @@ class Environment
 
 	/**
 	 * 返回当前的环境名称
+	 *
+	 * 首先检查是否指定了环境配置文件，如果存在则优先获取配置文件内容来设置当前运行环境。
+	 * 否则将检查$_SERVER环境变量，如果存在则使用该值进行设置。
+	 * 如果都没设置，则设为默认的production环境。
 	 * @return string
 	 */
 	public static function getEnvironment()
@@ -118,12 +137,14 @@ class Environment
 		if (!self::$environment) {
 			// 指定环境变量文件
 			if (!empty(self::$envFile) && is_file(self::$envFile)
-				&& self::isValid($env = file_get_contents(self::$envFile))) {
+				&& self::isValid($env = file_get_contents(self::$envFile))
+			) {
 				self::$environment = $env;
 			}
 			// 检查$_SERVER环境变量
 			if (!self::$environment && isset($_SERVER[self::$envVar])
-				&& self::isValid($_SERVER[self::$envVar])) {
+				&& self::isValid($_SERVER[self::$envVar])
+			) {
 				self::$environment = $_SERVER[self::$envVar];
 			} else {
 				self::$environment = self::getDefaultEnvironment();
