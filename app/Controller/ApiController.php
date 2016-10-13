535bda0300d828631765d43223532ae764a22a0e
sijie.li <lsj86@qq.com>
2016-10-13 17:04:36 +0800

    测试接口

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -15,6 +15,21 @@ class ApiController extends JsonController
 	public function infoAction()
 	{
 		$this->assign('info', ['id'=>1, 'name'=>'test']);
-		$this->display();
+		return $this->display();
 	}
+
+	public function testAction()
+	{
+		$data = [
+			'get' => $this->getQuery(),
+			'post' => $this->getPost(),
+			'headers' => $this->request->headers(),
+			'raw' => file_get_contents("php://input"),
+			'files' => $this->request->getFiles(),
+			'cookies' => $this->request->cookies()
+		];
+		$this->assign($data);
+		return $this->display();
+	}
+
 }
