31c5fad431b426ab0eed1b23f0d9a4dd8c8bd791
jesse.li <lsj86@qq.com>
2018-04-20 15:42:31 +0800

    psr7

---
+++ src/Core/Http/Stream.php
@@ -0,0 +1,195 @@
+<?php
+namespace Core\Http;
+
+use Psr\Http\Message\StreamInterface;
+
+class Stream implements StreamInterface
+{
+    private $stream;
+    private $readable;
+    private $writable;
+    private $seekable;
+    private $size;
+    private $uri;
+
+    private static $readWriteMode = [
+        'read' => [
+            'r' => true, 'w+' => true, 'r+' => true, 'x+' => true, 'c+' => true,
+            'rb' => true, 'w+b' => true, 'r+b' => true, 'x+b' => true,
+            'c+b' => true, 'rt' => true, 'w+t' => true, 'r+t' => true,
+            'x+t' => true, 'c+t' => true, 'a+' => true
+        ],
+        'write' => [
+            'w' => true, 'w+' => true, 'rw' => true, 'r+' => true, 'x+' => true,
+            'c+' => true, 'wb' => true, 'w+b' => true, 'r+b' => true,
+            'x+b' => true, 'c+b' => true, 'w+t' => true, 'r+t' => true,
+            'x+t' => true, 'c+t' => true, 'a' => true, 'a+' => true
+        ]
+    ];
+
+    public function __construct($stream)
+    {
+        if (!is_resource($stream)) {
+            throw new \InvalidArgumentException('stream must be a resource');
+        }
+        $this->stream = $stream;
+        $meta = stream_get_meta_data($stream);
+        $this->seekable = (bool)$meta['seekable'];
+        $this->readable = isset(self::$readWriteMode['read'][$meta['mode']]);
+        $this->writable = isset(self::$readWriteMode['write'][$meta['mode']]);
+        $this->uri = isset($meta['uri']) ? $meta['uri'] : null;
+    }
+
+    public function __toString()
+    {
+        try {
+            $this->seek(0);
+            $contents = (string)stream_get_contents($this->stream);
+            return $contents;
+        } catch (\Exception $e) {
+        }
+        return '';
+    }
+
+    public function close()
+    {
+        if (isset($this->stream)) {
+            if (is_resource($this->stream)) {
+                fclose($this->stream);
+            }
+            $this->detach();
+        }
+    }
+
+    public function detach()
+    {
+        if (!$this->stream) {
+            return null;
+        }
+        $result = $this->stream;
+        unset($this->stream);
+        $this->size = $this->uri = null;
+        $this->readable = $this->writable = $this->seekable = null;
+        return $result;
+    }
+
+    public function getSize()
+    {
+        if ($this->size !== null) {
+            return $this->size;
+        }
+        if (!$this->stream) {
+            return null;
+        }
+        if ($this->uri) {
+            clearstatcache(true, $this->uri);
+        }
+        $stats = fstat($this->stream);
+        if (isset($stats['size'])) {
+            $this->size = $stats['size'];
+            return $this->size;
+        }
+        return null;
+    }
+
+    public function tell()
+    {
+        $result = ftell($this->stream);
+        if ($result === false) {
+            throw new \RuntimeException('Unable to determine stream position');
+        }
+        return $result;
+    }
+
+    public function eof()
+    {
+        return !$this->stream || feof($this->stream);
+    }
+
+    public function isSeekable()
+    {
+        return $this->seekable;
+    }
+
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        if (!$this->seekable) {
+            throw new \RuntimeException('Stream is not seekable');
+        } elseif (fseek($this->stream, $offset, $whence) === -1) {
+            throw new \RuntimeException('Unable to seek to stream position '
+                . $offset . ' with whence ' . var_export($whence, true));
+        }
+    }
+
+    public function rewind()
+    {
+        $this->seek(0);
+    }
+
+    public function isWritable()
+    {
+        return $this->writable;
+    }
+
+    public function write($string)
+    {
+        if (!$this->writable) {
+            throw new \RuntimeException('Cannot write to a non-writable stream');
+        }
+        $this->size = null;
+        $result = fwrite($this->stream, $string);
+        if ($result === false) {
+            throw new \RuntimeException('Unable to write to stream');
+        }
+        return $result;
+    }
+
+    public function isReadable()
+    {
+        return $this->readable;
+    }
+
+    public function read($length)
+    {
+        if (!$this->readable) {
+            throw new \RuntimeException('Cannot read from non-readable stream');
+        }
+        if ($length < 0) {
+            throw new \RuntimeException('Length parameter cannot be negative');
+        }
+        if (0 === $length) {
+            return '';
+        }
+        $string = fread($this->stream, $length);
+        if (false === $string) {
+            throw new \RuntimeException('Unable to read from stream');
+        }
+
+        return $string;
+    }
+
+    public function getContents()
+    {
+        $contents = stream_get_contents($this->stream);
+        if ($contents === false) {
+            throw new \RuntimeException('Unable to read stream contents');
+        }
+        return $contents;
+    }
+
+    public function getMetadata($key = null)
+    {
+        if (!$this->stream) {
+            return $key ? '' : [];
+        } elseif (!$key) {
+            return stream_get_meta_data($this->stream);
+        }
+        $meta = stream_get_meta_data($this->stream);
+        return isset($meta[$key]) ? $meta[$key] : null;
+    }
+
+    public function __destruct()
+    {
+        $this->close();
+    }
+}
---
+++ src/Core/Http/UploadedFile.php
@@ -0,0 +1,176 @@
+<?php
+namespace Core\Http;
+
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+
+class UploadedFile implements UploadedFileInterface
+{
+
+    private static $errors = [
+        UPLOAD_ERR_OK,
+        UPLOAD_ERR_CANT_WRITE,
+        UPLOAD_ERR_EXTENSION,
+        UPLOAD_ERR_FORM_SIZE,
+        UPLOAD_ERR_INI_SIZE,
+        UPLOAD_ERR_NO_FILE,
+        UPLOAD_ERR_NO_TMP_DIR,
+        UPLOAD_ERR_PARTIAL,
+    ];
+
+    /**
+     * 上传到服务器的文件路径
+     * @var string
+     */
+    private $file;
+
+    /**
+     * 文件名
+     * @var string
+     */
+    private $name;
+
+    /**
+     * 文件类型
+     * @var string
+     */
+    private $type;
+
+    /**
+     * 文件大小
+     * @var int
+     */
+    private $size;
+
+    /**
+     * 错误码
+     * @var int
+     */
+    private $error;
+
+    /**
+     * 是否SAPI
+     * @var bool
+     */
+    private $sapi;
+
+    /**
+     * @var StreamInterface
+     */
+    private $stream;
+
+    /**
+     * 是否已经移走
+     * @var bool
+     */
+    private $moved = false;
+
+    public function __construct($file, $name = null, $type = null, $size = null, $error = UPLOAD_ERR_OK, $sapi = false)
+    {
+        if (!in_array($error, self::$errors)) {
+            throw new \InvalidArgumentException('Invalid error status.');
+        }
+        if (null !== $size && false === is_int($size)) {
+            throw new \InvalidArgumentException('Upload file size must be an integer');
+        }
+        $this->file = $file;
+        $this->name = $name;
+        $this->type = $type;
+        $this->size = $size;
+        $this->error = $error;
+        $this->sapi = $sapi;
+    }
+
+    /**
+     * 获取上传文件的流对象
+     *
+     * @return StreamInterface
+     * @throws \RuntimeException
+     */
+    public function getStream()
+    {
+        if ($this->moved) {
+            throw new \RuntimeException(sprintf('Uploaded file %1s has already been moved', $this->name));
+        }
+        if ($this->stream === null) {
+            $this->stream = new Stream(fopen($this->file, 'r'));
+        }
+        return $this->stream;
+    }
+
+    /**
+     * 移动上传文件
+     *
+     * @param string $targetPath
+     */
+    public function moveTo($targetPath)
+    {
+        if ($this->moved) {
+            throw new \RuntimeException('Uploaded file already moved');
+        }
+        $targetIsStream = strpos($targetPath, '://') > 0;
+        if (!$targetIsStream && !is_writable(dirname($targetPath))) {
+            throw new \RuntimeException('Upload target path is not writable');
+        }
+        if ($targetIsStream) {
+            if (!copy($this->file, $targetPath)) {
+                throw new \RuntimeException("Error moving uploaded file {$this->name} to {$targetPath}");
+            }
+            if (!unlink($this->file)) {
+                throw new \RuntimeException("Error removing uploaded file {$this->file}");
+            }
+        } elseif ($this->sapi) {
+            if (!is_uploaded_file($this->file)) {
+                throw new \RuntimeException("{$this->file} is not a valid uploaded file");
+            }
+            if (!move_uploaded_file($this->file, $targetPath)) {
+                throw new \RuntimeException("Error moving uploaded file {$this->name} to {$targetPath}");
+            }
+        } else {
+            if (!rename($this->file, $targetPath)) {
+                throw new \RuntimeException("Error moving uploaded file {$this->name} to {$targetPath}");
+            }
+        }
+        $this->moved = true;
+    }
+
+    /**
+     * 获取上传文件大小
+     *
+     * @return int|null
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * 获取错误码
+     *
+     * @return int
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * 获取源文件名
+     *
+     * @return string|null
+     */
+    public function getClientFilename()
+    {
+        return $this->name;
+    }
+
+    /**
+     * 获取媒体类型
+     *
+     * @return string|null
+     */
+    public function getClientMediaType()
+    {
+        return $this->type;
+    }
+}
