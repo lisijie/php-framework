ec49f4844cf0d756a2b5a1718ac692e8db697e0d
jesse.li <lsj86@qq.com>
2018-05-10 11:48:36 +0800

    优化

--- src/Core/Web/Debug/Middleware/DebuggerMiddleware.php
+++ src/Core/Web/Debug/Middleware/DebuggerMiddleware.php
@@ -20,41 +20,39 @@ class DebuggerMiddleware implements MiddlewareInterface
 
     public function __construct()
     {
-        if (!App::isCli()) {
-            if (extension_loaded('xhprof')) {
-                $this->profileExtension = 'xhprof';
-                xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_NO_BUILTINS);
-            } elseif (extension_loaded('tideways_xhprof')) {
-                $this->profileExtension = 'tideways_xhprof';
-                tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_MEMORY_MU | TIDEWAYS_XHPROF_FLAGS_MEMORY_PMU | TIDEWAYS_XHPROF_FLAGS_CPU);
-            }
-            Events::on(Db::class, Db::EVENT_QUERY, function (DbEvent $event) {
-                $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
-                $service = $model = $controller = '';
-                foreach ($backtrace as $item) {
-                    if (isset($item['file'])) {
-                        if (strpos($item['file'], 'lisijie/framework') !== false) {
-                            continue;
-                        }
-                        if (!$service && substr($item['file'], -11) == 'Service.php') {
-                            $service = basename($item['file']) . ":{$item['line']}";
-                        } elseif (!$model && substr($item['file'], -9) == 'Model.php') {
-                            $model = basename($item['file']) . ":{$item['line']}";
-                        } elseif (!$controller && substr($item['file'], -14) == 'Controller.php') {
-                            $controller = basename($item['file']) . ":{$item['line']}";
-                        }
+        if (extension_loaded('xhprof')) {
+            $this->profileExtension = 'xhprof';
+            xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_NO_BUILTINS);
+        } elseif (extension_loaded('tideways_xhprof')) {
+            $this->profileExtension = 'tideways_xhprof';
+            tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_MEMORY_MU | TIDEWAYS_XHPROF_FLAGS_MEMORY_PMU | TIDEWAYS_XHPROF_FLAGS_CPU);
+        }
+        Events::on(Db::class, Db::EVENT_QUERY, function (DbEvent $event) {
+            $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
+            $service = $model = $controller = '';
+            foreach ($backtrace as $item) {
+                if (isset($item['file'])) {
+                    if (strpos($item['file'], 'lisijie/framework') !== false) {
+                        continue;
+                    }
+                    if (!$service && substr($item['file'], -11) == 'Service.php') {
+                        $service = basename($item['file']) . ":{$item['line']}";
+                    } elseif (!$model && substr($item['file'], -9) == 'Model.php') {
+                        $model = basename($item['file']) . ":{$item['line']}";
+                    } elseif (!$controller && substr($item['file'], -14) == 'Controller.php') {
+                        $controller = basename($item['file']) . ":{$item['line']}";
                     }
                 }
-                $this->sqlLogs[] = [
-                    'service' => $service,
-                    'model' => $model,
-                    'controller' => $controller,
-                    'time' => $event->getTime() * 1000000, // 微秒
-                    'sql' => $event->getSql(),
-                    'params' => $event->getParams(),
-                ];
-            });
-        }
+            }
+            $this->sqlLogs[] = [
+                'service' => $service,
+                'model' => $model,
+                'controller' => $controller,
+                'time' => $event->getTime() * 1000000, // 微秒
+                'sql' => $event->getSql(),
+                'params' => $event->getParams(),
+            ];
+        });
     }
 
     /**
@@ -67,8 +65,7 @@ class DebuggerMiddleware implements MiddlewareInterface
     public function process(Request $request, callable $next)
     {
 
-        if (App::isCli()
-            || substr(CUR_ROUTE, 0, 6) == 'debug/'
+        if (substr(CUR_ROUTE, 0, 6) == 'debug/'
             || (!$request->getCookieParam(Config::$startCookieName) && !$request->getQueryParam(Config::$debugParamName))
         ) {
             return $next();
